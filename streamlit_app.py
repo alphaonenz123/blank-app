caf0fde200457e6edd0b2175798960a4bb278fa9
alphaonenz123 <50907554+alphaonenz123@users.noreply.github.com>
2024-07-20 21:55:09 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ except ImportError as e:
     sys.exit(1)
 
 try:
-    import folium
+    import streamlit-folium
     from folium.plugins import Draw
 except ImportError as e:
     print(f"Error importing folium: {e}")
